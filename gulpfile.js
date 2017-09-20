64ecc0cfc258037abd1cd6d49c2c438c4fc7af2e
Rodrigo Waltenberg <rwaltenberg@gmail.com>
2017-09-20 10:24:51 -0300

    Inclusão de linting no javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,12 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')
-const sourcemaps = require('gulp-sourcemaps')
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('sass', function() {
-    return gulp.src('./src/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(
-            sass({outputStyle: 'expanded'})
-            .on('error', sass.logError)
-        )
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./dist'))
-})
+gulp.task('sass', () =>
+  gulp.src('./src/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist')));
 
-gulp.task('watch', function() {
-    return gulp.watch('./src/**/*.scss', ['sass'])
-})
+gulp.task('watch', () => gulp.watch('./src/**/*.scss', ['sass']));
